404d94b94d819754e1c05a9b0aa3dba038fdde8f
partg952 <dankparth@gmail.com>
2022-01-07 10:09:17 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,42 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-import React from 'react';
-import axios from 'axios';
-import Navbar from './components/Navbar';
-import Main from './components/Main';
-import Info from './components/Info';
-import Genres from './components/Genres';
-import {BrowserRouter as Router,Route,useParams} from 'react-router-dom';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import axios from "axios";
+import Navbar from "./components/Navbar";
+import Main from "./components/Main";
+import Info from "./components/Info";
+import Genres from "./components/Genres";
+import { BrowserRouter as Router, Route, useParams } from "react-router-dom";
 function App() {
-const [data,setData] = React.useState([]);
-// console.log(data);
-React.useEffect(()=>{
-  for(let i=0;i<5;i++){
-    axios('https://anime5311.herokuapp.com/api/popular/'+i)
-    .then((res)=>{
-      // console.log(res.data);
-      res.data.results.forEach((item)=>{
-        setData(prev=>[...prev,item]);
-      })
-      
-      // console.log(data)
-    })
-  }
-},[])
+  const [data, setData] = React.useState([]);
+  // console.log(data);
+  React.useEffect(() => {
+    for (let i = 0; i < 5; i++) {
+      axios("https://anime5311.herokuapp.com/api/popular/" + i).then((res) => {
+        // console.log(res.data);
+        res.data.results.forEach((item) => {
+          setData((prev) => [...prev, item]);
+        });
+
+        // console.log(data)
+      });
+    }
+  }, []);
   return (
     <div className="App">
       <Router>
-     <Navbar data={data} setData={setData}/>
-     <Genres setData={setData}/>
-     <Route path='/' exact >
-    <Main data={data}/>
-     </Route>
-     <Route path='/info/:id' exact>
-        <Info/>
-     </Route>
+        <Navbar data={data} setData={setData} />
+        <Genres setData={setData} />
+        <Route path="/" exact>
+          <p id="recently-watched-episode">
+            Recently Watched Anime:{localStorage.getItem("anime_name")} EP:
+            {localStorage.getItem("episode_number")}
+          </p>
+          <Main data={data} />
+        </Route>
+        <Route path="/info/:id" exact>
+          <Info />
+        </Route>
       </Router>
-    </div>  
-
+    </div>
   );
 }
 
